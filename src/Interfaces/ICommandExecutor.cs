230a5de45f43dc24d07ef48c70bfc30834f90a9b
okertanov <okertanov@gmail.com>
2014-07-04 18:34:35 +0300

    Command Executors added, first working items

---
+++ src/Interfaces/ICommandExecutor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Actionix
+{
+	public interface ICommandExecutor
+	{
+		void Execute(ICommandContext context);
+		void Execute(string context);
+	}
+}
+
--- src/MenuBuilders/StaticMenuItemsBuilder.cs
+++ src/MenuBuilders/StaticMenuItemsBuilder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using MonoMac.AppKit;
+using System.Collections.Generic;
 
 namespace Actionix
 {
@@ -8,16 +10,34 @@ namespace Actionix
 	//
 	public class StaticMenuItemsBuilder : IMenuItemsBuilder
 	{
+		private static readonly ShellCommandExecutor _shellCommandExecutor = new ShellCommandExecutor();
+		private static readonly ApplicationCommandExecutor _applicationCommandExecutor = new ApplicationCommandExecutor();
+
+		private static readonly Dictionary<string, Action> MenuItems = new Dictionary<string, Action>()
+		{
+			{ "Vim",			() => _applicationCommandExecutor.Execute("MacVim.app") 	},
+			{ "Chrome",			() => _applicationCommandExecutor.Execute("Google Chrome.app")	},
+			{ "Terminal",		() => _applicationCommandExecutor.Execute("Terminal.app")	},
+			{ "Lock Screen",	() => _shellCommandExecutor.Execute("/System/Library/CoreServices/Menu%20Extras/user.menu/Contents/Resources/CGSession -suspend") 		}
+		};
+
 		public void AttachTo(NSMenu menu)
 		{
-			menu.AddItem(new NSMenuItem("Vim", "", OnMenu));
-			menu.AddItem(new NSMenuItem("Chrome", "", OnMenu));
-			menu.AddItem(new NSMenuItem("Terminal", "", OnMenu));
-			menu.AddItem(new NSMenuItem("Lock Screen", "", OnMenu));
+			MenuItems.ToList().ForEach(item => menu.AddItem(new NSMenuItem(item.Key, "", OnMenu)));
 		}
 
 		private void OnMenu(object sender, EventArgs args)
 		{
+			var item = sender as NSMenuItem;
+
+			if (item != null)
+			{
+				var action = MenuItems[item.Title];
+				if (action != null)
+				{
+					action.Invoke();
+				}
+			}
 		}
 	}
 }
