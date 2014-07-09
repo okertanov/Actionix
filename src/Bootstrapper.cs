339119f6e18e43742505d22e473b866f32b33502
okertanov <okertanov@gmail.com>
2014-07-10 01:48:56 +0300

    style, static are in upper case

--- src/Bootstrapper.cs
+++ src/Bootstrapper.cs
@@ -8,28 +8,28 @@ namespace Actionix
 {
 	public static class Bootstrapper
 	{
-		private static readonly TinyIoCContainer container = TinyIoCContainer.Current;
+		private static readonly TinyIoCContainer Container = TinyIoCContainer.Current;
 
 		public static void Register()
 		{
-			container.Register<ITinyMessengerHub, TinyMessengerHub>().AsSingleton();
+			Container.Register<ITinyMessengerHub, TinyMessengerHub>().AsSingleton();
 
-			container.Register<IApplicationCommandExecutor, ApplicationCommandExecutor>().AsSingleton();
-			container.Register<ISelectorCommandExecutor, SelectorCommandExecutor>().AsSingleton();
-			container.Register<IShellCommandExecutor, ShellCommandExecutor>().AsSingleton();
+			Container.Register<IApplicationCommandExecutor, ApplicationCommandExecutor>().AsSingleton();
+			Container.Register<ISelectorCommandExecutor, SelectorCommandExecutor>().AsSingleton();
+			Container.Register<IShellCommandExecutor, ShellCommandExecutor>().AsSingleton();
 
-			container.Register<IMenuExtraModule, MenuExtraModule>().AsSingleton();
+			Container.Register<IMenuExtraModule, MenuExtraModule>().AsSingleton();
 		}
 
 		public static void Bootstrap()
 		{
-			var extraModule = container.Resolve<IMenuExtraModule>();
+			var extraModule = Container.Resolve<IMenuExtraModule>();
 			extraModule.Run();
 		}
 
 		public static void Terminate()
 		{
-			var hub = container.Resolve<ITinyMessengerHub>();
+			var hub = Container.Resolve<ITinyMessengerHub>();
 			var msg = new AppBeforeExitMessage(NSApplication.SharedApplication, null);
 			hub.Publish(msg);
 		}
