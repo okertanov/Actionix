e5f56c139f8e7e6ea4659e12c00f19f92544657d
okertanov <okertanov@gmail.com>
2014-07-14 14:39:53 +0300

    fs helper: initial unique name by pattern generator

--- src/Helpers/FileSystemHelper.cs
+++ src/Helpers/FileSystemHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Actionix
 {
@@ -20,14 +21,30 @@ namespace Actionix
 
 		public static void CreateNewFile(string name)
 		{
-			System.IO.File.Create(name);
+			File.Create(name);
 		}
 
-		public static string GenerateUniqueFileNameAt<T>(string path, string nameMask, IEnumerator<T> uniquePartGenerator)
+		public static string GenerateNonExistentFileNameAt<T>(string path, string nameMask, IEnumerator<T> uniquePartGenerator)
 		{
-			var fileName = String.Empty;
+			var fileName = String.Format(nameMask, uniquePartGenerator.Current);
+			var filePath = Path.Combine(path, fileName);
 
-			return fileName;
+			if (File.Exists(filePath)) {
+				var count = 0;
+
+				while (uniquePartGenerator.MoveNext())
+				{
+					fileName = String.Format(nameMask, uniquePartGenerator.Current);
+					filePath = Path.Combine(path, fileName);
+
+					if (count++ > MaxUniqueFsNameProbes)
+					{
+						break;
+					}
+				}
+			}
+
+			return filePath;
 		}
 	}
 }
