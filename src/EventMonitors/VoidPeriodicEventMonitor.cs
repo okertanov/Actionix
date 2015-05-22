e5211674facf252dac531df2245a3c3e7a6e3ebd
okertanov <okertanov@gmail.com>
2015-05-22 15:32:10 +0300

    Leaky PeriodicEventMonitor disabled for now 2

---
+++ src/EventMonitors/VoidPeriodicEventMonitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MonoMac.AppKit;
+using System.Threading;
+using MonoMac.Foundation;
+
+namespace Actionix
+{
+	public class VoidPeriodicEventMonitor : IPeriodicEventMonitor
+	{
+		public void Activate (Action<NSEvent> onEventMonitorAction)
+		{
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+		}
+
+		~VoidPeriodicEventMonitor()
+		{
+			Dispose(false);
+		}
+	}
+}
+
