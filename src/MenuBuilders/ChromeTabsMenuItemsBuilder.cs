86514716945536820440bfa150021bb9c74e07e9
Oleg Kertanov <okertanov@gmail.com>
2017-07-14 16:26:01 +0300

    Semi-working tabs rendering 2

--- src/MenuBuilders/ChromeTabsMenuItemsBuilder.cs
+++ src/MenuBuilders/ChromeTabsMenuItemsBuilder.cs
@@ -69,16 +69,16 @@ namespace Actionix.MenuBuilders {
 			}
 
 			public override void MenuWillOpen(NSMenu menu) {
-				//Console.WriteLine(String.Format("MenuWillOpen: for menu: {0}", menu.Title));
+				Console.WriteLine(String.Format("MenuWillOpen: for menu: {0}", menu.Title));
 			}
 
 			public override void MenuDidClose(NSMenu menu) {
-				//Console.WriteLine(String.Format("MenuDidClose: for menu: {0}", menu.Title));
+				Console.WriteLine(String.Format("MenuDidClose: for menu: {0}", menu.Title));
 			}
 
 			public override int MenuItemCount(NSMenu menu) {
 				var tabsNumber = parentRef.Target.GetTabsNumber(this);
-				//Console.WriteLine(String.Format("MenuItemCount: for menu: {0}, tabs: {1}", menu.Title, tabsNumber));
+				Console.WriteLine(String.Format("MenuItemCount: for menu: {0}, tabs: {1}", menu.Title, tabsNumber));
 				return tabsNumber;
 			}
 
@@ -87,22 +87,22 @@ namespace Actionix.MenuBuilders {
 			}
 
 			public override void NeedsUpdate(NSMenu menu) {
-				//Console.WriteLine(String.Format("NeedsUpdate: for menu: {0}", menu.Title));
+				Console.WriteLine(String.Format("NeedsUpdate: for menu: {0}", menu.Title));
 				parentRef.Target.UpdateTabs(this);
 			}
 
 			public override bool UpdateItem(NSMenu menu, NSMenuItem item, int atIndex, bool shouldCancel) {
-				//Console.WriteLine(String.Format("UpdateItem: for menu: {0}, item: {1}, atIndex: {2}, shouldCancel: {3}", menu.Title, item.Title, atIndex, shouldCancel));
+				Console.WriteLine(String.Format("UpdateItem: for menu: {0}, item: {1}, atIndex: {2}, shouldCancel: {3}", menu.Title, item.Title, atIndex, shouldCancel));
 				return base.UpdateItem(menu, item, atIndex, shouldCancel);
 			}
 
 			public override RectangleF ConfinementRectForMenu(NSMenu menu, NSScreen screen) {
-				//Console.WriteLine(String.Format("ConfinementRectForMenu: for menu: {0}, screen: {1}", menu.Title, screen.DeviceDescription));
+				Console.WriteLine(String.Format("ConfinementRectForMenu: for menu: {0}, screen: {1}", menu.Title, screen.DeviceDescription));
 				return base.ConfinementRectForMenu(menu, screen);
 			}
 
 			public override bool HasKeyEquivalentForEvent(NSMenu menu, NSEvent theEvent, NSObject target, Selector action) {
-				//Console.WriteLine(String.Format("HasKeyEquivalentForEvent: for menu: {0}, event: {1}, target: {2}", menu.Title, theEvent, target));
+				Console.WriteLine(String.Format("HasKeyEquivalentForEvent: for menu: {0}, event: {1}, target: {2}", menu.Title, theEvent, target));
 				return base.HasKeyEquivalentForEvent(menu, theEvent, target, action);
 			}
 		}
